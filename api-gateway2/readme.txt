c196be9c590a37c4f30b7ad50509b658c1f37960
ajilisiwei <ajilisiwei@126.com>
2018-04-08 23:36:36 +0800

    请求过滤

--- api-gateway2/readme.txt
+++ api-gateway2/readme.txt
@@ -2,3 +2,18 @@
 2.请求url分别为：
     api-a: http://desktop-flis9e6:5555/api-a/hello
     api-b: http://desktop-flis9e6:5555/api-b/feign-consumer
+3.服务路由配置
+   (1).简写
+        zuul.routes.hello-service=/hello-service/**
+        等效于：
+        zuul.routes.hello-service.path=/hello-service/**
+        zuul.routes.hello-service.serviceId=hello-service
+   (2).服务路由的默认规则
+        zuul.routes.hello-service.path=/hello-service/**
+        zuul.routes.hello-service.serviceId=hello-service
+        这是Eureka服务注册机制为每个已注册服务添加的一个默认路由规则，此时所有的服务均被添加了映射关系，如不希望服务被外界访问，
+        可添加zuul.ignored-services参数来忽略它，如zuul.ignored-services=*则表示所有的注册服务均不创建默认路由映射规则
+4.增加请求过滤
+   (1).增加AccessFilter类并继承ZuulFilter类
+   (2).重写run方法,并实现对请求的拦截和过滤
+   (3).改写应用主类，为过滤类创建具体的Bean
--- api-gateway2/src/main/java/com/wei/zuul/Application.java
+++ api-gateway2/src/main/java/com/wei/zuul/Application.java
@@ -1,13 +1,24 @@
 package com.wei.zuul;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    //01.没有增加路由过滤的代码
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+
+    public static void main(String[] args){
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
